8eef205dd26fb05ca835d8238a3091ec040d4459
Rainweic <861514706@qq.com>
2022-07-21 13:56:30 +0800

    [update] pass test

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use ndarray::prelude::*;
+
+// Test
+fn main() {
+    let a = array![
+                [1.,2.,3.],
+                [4.,5.,6.],
+            ];
+    assert_eq!(a.ndim(), 2);         // get the number of dimensions of array a
+    assert_eq!(a.len(), 6);          // get the number of elements in array a
+    assert_eq!(a.shape(), [2, 3]);   // get the shape of array a
+    assert_eq!(a.is_empty(), false); // check if the array has zero elements
+
+    println!("{:?}", a);
+}
